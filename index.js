436191f28ebbf7e32ad96b273842e6a60d484b6f
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2023-08-14 18:36:55 +0100

    working upload onchange and display onclick

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
+var readerResult;
+
 function viewFileData() {
-    var file    = document.querySelector('input[type=file]').files[0];
-    var reader  = new FileReader();
-  
-    // Get blob?
-    if(file instanceof Blob) {
-        console.log("True");
-    } else {
-        console.log("False");
-    }
-    console.log(typeof file);
-    let fileName = `${Date.now()}-htp-${file.name}`;
-    console.log("file name is:" + fileName);
+  var file = document.getElementById('fileInput').files[0];
+  function getBase64(file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      readerResult = reader.result;
+      console.log(reader.result);
+    };
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };
   }
+  
+  getBase64(file);
+  console.log(readerResult);
+
+  /** code to test type of file
+  if (file instanceof Blob) {
+    console.log("True");
+  } else {
+    console.log("False");
+  }
+  */
+  
+  /** code to create unique filename
+  let fileName = `${Date.now()}-htp-${file.name}`;
+  console.log("file name is:" + fileName);
+  console.log("reader result is:");
+  console.log(`${readerResult}`);
+  */
+}
+
+function addImg() {
+  const newImg = document.createElement("img");
+  newImg.src = readerResult;
+  newImg.alt = "alt text";
+  document.getElementById("img1").appendChild(newImg);
+}
