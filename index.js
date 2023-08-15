def2a8ab9f7b20d1b2bacf72edf982de8a8af884
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2023-08-15 08:13:46 +0100

    javascript cleanup

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
+// variable to store and pass image BLOB data between upload and display functions
 var readerResult;
 
-function viewFileData() {
+// upload image BLOB data from HTML file select
+function uploadFileData() {
   var file = document.getElementById('fileInput').files[0];
   function getBase64(file) {
     var reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = function () {
       readerResult = reader.result;
+      // check file data has been read
       console.log(reader.result);
     };
     reader.onerror = function (error) {
+      // report any error which has occurred
       console.log('Error: ', error);
     };
   }
   getBase64(file);
-  console.log(readerResult);
-
-  /** code to test type of file
-  if (file instanceof Blob) {
-    console.log("True");
-  } else {
-    console.log("False");
-  }
-  */
   
-  /** code to create unique filename
-  let fileName = `${Date.now()}-htp-${file.name}`;
-  console.log("file name is:" + fileName);
-  console.log("reader result is:");
-  console.log(`${readerResult}`);
+  /** optional code to create unique filename
+  let fileName = `${Date.now()}-optionalText-${file.name}`;
   */
 }
 
-function addImg() {
+// display BLOB image data in HTML document
+function viewImg() {
   const newImg = document.createElement("img");
   newImg.src = readerResult;
   newImg.alt = "alt text";
